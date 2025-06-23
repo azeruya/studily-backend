05d51d98823927651007dabdd28c3c05c33e83d9
Fathiya Azelya <fathiyaazelya@Fathiyas-MacBook-Pro.local>
2025-06-23 20:10:15 +0800

    Fix CORS middleware to add headers

--- src/Application/Middleware/CorsMiddleware.php
+++ src/Application/Middleware/CorsMiddleware.php
@@ -2,29 +2,29 @@
 
 namespace App\Application\Middleware;
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Psr7\Response;
 
-class CorsMiddleware
+class CorsMiddleware implements MiddlewareInterface
 {
-    public function __invoke(Request $request, RequestHandler $handler): Response
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        // Handle preflight requests
+        $origin = $_ENV['FRONTEND_URL'] ?? '*';
+
         if ($request->getMethod() === 'OPTIONS') {
-            $response = new \Slim\Psr7\Response();
-            return $this->addCorsHeaders($response);
+            $response = new Response();
+        } else {
+            $response = $handler->handle($request);
         }
 
-        $response = $handler->handle($request);
-        return $this->addCorsHeaders($response);
-    }
-
-    private function addCorsHeaders(Response $response): Response
-    {
+        // Attach headers in both cases
         return $response
-            ->withHeader('Access-Control-Allow-Origin', '*') // Use specific domain for production
+            ->withHeader('Access-Control-Allow-Origin', $origin)
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->withHeader('Access-Control-Allow-Credentials', 'true');
     }
 }

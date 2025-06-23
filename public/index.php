d7fff4259a9adf003d1c858a13a720983d5b8356
Fathiya Azelya <fathiyaazelya@Fathiyas-MacBook-Pro.local>
2025-06-23 19:03:44 +0800

    Fix: skip dotenv load on production

--- public/index.php
+++ public/index.php
@@ -12,8 +12,10 @@ use Slim\Factory\ServerRequestCreatorFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+}
 
 // Instantiate PHP-DI ContainerBuilder
 $containerBuilder = new ContainerBuilder();

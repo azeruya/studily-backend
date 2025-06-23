fa1e81a013db5977286446ba98ad2f928982e5bb
Fathiya Azelya <fathiyaazelya@Fathiyas-MacBook-Pro.local>
2025-06-23 19:51:38 +0800

    Add CORS middleware

---
+++ src/Application/Middleware/CorsMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Application\Middleware;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class CorsMiddleware
+{
+    public function __invoke(Request $request, RequestHandler $handler): Response
+    {
+        // Handle preflight requests
+        if ($request->getMethod() === 'OPTIONS') {
+            $response = new \Slim\Psr7\Response();
+            return $this->addCorsHeaders($response);
+        }
+
+        $response = $handler->handle($request);
+        return $this->addCorsHeaders($response);
+    }
+
+    private function addCorsHeaders(Response $response): Response
+    {
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*') // Use specific domain for production
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    }
+}
